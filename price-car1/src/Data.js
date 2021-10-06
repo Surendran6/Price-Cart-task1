0018f7015ee674dd4202c9db5f55f1e06b9c0a2e
Surendran6 <88974825+Surendran6@users.noreply.github.com>
2021-10-06 15:05:04 +0530

    Add files via upload

---
+++ price-car1/src/Data.js
@@ -0,0 +1,50 @@
+export const data =[{
+    title:"Free",
+    price:"$0",
+    accessAble:[
+        "Single User", 
+        "5GB Storage",
+        "Unlimited Public Projects",
+        "Community Access"
+    ],
+    unaccessAble:[
+        "Unlimited Private Projects",
+        "Dedicated Phone Support",
+        "Free Subdomain",
+        "Monthly Status Reports"
+    ]                  
+},
+{
+    title:"Plus",
+    price:"$9",
+    accessAble:[
+        "5 Users",
+        "50GB Storage",
+        "Unlimited Public Projects",
+        "Community Access",
+        "Unlimited Private Projects",
+        "Dedicated Phone Support",
+        "Free Subdomain"
+    ],
+    unaccessAble:[
+        "Monthly Status Reports"
+    ]
+},
+{
+    title:"Pro",
+    price:"$49",
+    accessAble:[
+        "Unlimited Users",
+        "150GB Storage",
+        "Unlimited Public Projects",
+        "Community Access",
+        "Unlimited Private Projects",
+        "Dedicated Phone Support",
+        "Unlimited Free Subdomains",
+        "Monthly Status Reports"
+    ],
+    unaccessAble:[]
+}
+]
+
+
